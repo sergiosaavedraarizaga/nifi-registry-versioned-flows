0dba58b660ecb7367a43ae5b641c95bfc2be3c12
sergiosaavedraarizaga <88031016+sergiosaavedraarizaga@users.noreply.github.com>
2022-09-27 10:52:13 +0200

    Update cicd2.sh

--- scripts/cicd2.sh
+++ scripts/cicd2.sh
@@ -1,7 +1,22 @@
 #!/usr/bin/bash -x
 
+# Install packages and nifi-toolkit
+sudo apt-get update
+sudo apt-get install jq
+sudo apt-get install openjdk-11-jdk
+sudo apt-get install wget
+sudo apt-get install unzip
+sudo sh -c "cd /opt; wget https://archive.apache.org/dist/nifi/1.16.3/nifi-toolkit-1.16.3-bin.zip"
+sudo sh -c "cd /opt; unzip nifi-toolkit-1.16.3-bin.zip; chmod -R 755 /opt"
+nifipath="/opt/nifi-toolkit-1.16.3"
+sourcenifi="http://52.208.110.211:8443"
+sourceregistry="http://3.252.41.20:18080" # Lab Registry. Later we will assign this value as a github secret variable
+export JAVA_HOME=/usr/lib/jvm/java-1.11.0-openjdk-amd64
+
 # VARIABLES
 echo ${GITHUB_REF##/}
+# Charge .properties, it will choose a file but depends of the $env value that change and depends of the current branch. 
+# It also assign $targetenv and $targetnifi variables.
 case ${GITHUB_REF##*/} in
   feature*)
     env=dev; echo $env
@@ -17,73 +32,36 @@ case ${GITHUB_REF##*/} in
     source ./vars/setup_${env}.properties;;
   *) echo "Error getting branch name"; exit 1 ;;
  
- if [[ -z ${target_nifi ]]
+# If the variable target_nifi is empty then the script finish, the one that is assigned into the .properties file
+if [[ -z ${target_nifi ]]
   then
     echo "target_nifi variable is not configured"
     exit 1
  fi
  
- # Charge environment variables from vars/setup.json
-flow_name=$(eval cat ./vars/flow.json | jq '.[].flow_name')
-flow_version=$(cat ./vars/flow.json | jq '.[].flow_version')
+ # Charge environment variables from vars/setup.json file
+num=$(cat ./vars/flow.json | jq length)
+# If flow.json file only has 1 flow_name and 1 flow_version then it assigns values without a loop
+if [[ $num -eq 1 ]]
+  then
+    flow_name=$(eval cat ./vars/flow.json | jq '.[].flow_name')
+    flow_version=$(cat ./vars/flow.json | jq '.[].flow_version')
+fi
 
-sourceenv=lab
-nifipath="/opt/nifi-toolkit-1.16.3"
-#sourcenifi="http://52.208.110.211:8443"
-#targetnifi="http://54.220.59.197:8443"
-sourceregistry="http://3.252.41.20:18080" # Lab Registry
-#targetregistry="http://54.171.38.213:18080" # Dev Registry
+sourceflowname=$flow_name
+sourceflowversion=$flow_version
+sourceflowid=$(${nifipath}/bin/cli.sh nifi pg-list -u ${sourcenifi} -ot json | jq '.[] | select(.name=="'"$flowname"'").versionControlInformation.flowId')
 
-
-sudo apt-get update
-sudo apt-get install jq
-sudo apt-get install openjdk-11-jdk
-sudo apt-get install wget
-sudo apt-get install unzip
-sudo sh -c "cd /opt; wget https://archive.apache.org/dist/nifi/1.16.3/nifi-toolkit-1.16.3-bin.zip"
-sudo sh -c "cd /opt; unzip nifi-toolkit-1.16.3-bin.zip; chmod -R 755 /opt"
-export JAVA_HOME=/usr/lib/jvm/java-1.11.0-openjdk-amd64
-# Get Bucket Id from source (dev bucket)
-sourcebucketid=$(${nifipath}/bin/cli.sh registry list-buckets -u ${sourceregistry} -ot json | jq '.[] | select(.name|test("'"${sourceenv}"'")).identifier')
-sourceflows=$(${nifipath}/bin/cli.sh registry list-flows -b ${sourcebucketid} -u ${sourceregistry} -ot json | jq '.[].identifier')
-# Get pgId using the flowId obteined in the registry
-sourceflowid=$(echo $sourceflows | sed 's/\"//g')
-for sourceflow in $sourceflowid
-do
-        sourceflowversion=$(${nifipath}/bin/cli.sh nifi pg-list -u ${sourcenifi} -ot json | jq '.[] | select(.versionControlInformation.flowId=="'"$sourceflow"'").versionControlInformation.version')
-        sourceflowname=$(${nifipath}/bin/cli.sh nifi pg-list -u ${sourcenifi} -ot json | jq '.[] | select(.versionControlInformation.flowId=="'"$sourceflow"'").name')
-        sourceflowname=$(echo $sourceflowname | sed 's/\"//g')
-
-        ###################################
-        # Export flows from source registry
-        ###################################
-        > flow.json
-
-        ${nifipath}/bin/cli.sh registry export-flow-version -f ${sourceflow} -fv ${sourceflowversion} -o flow.json -u ${sourceregistry}
-
-        # Import flow from json file
-        # To import a flow we need to create a new flow in the target registry
-
-        # Getting Target bucket Id
-        targetbucketid=$(${nifipath}/bin/cli.sh registry list-buckets -u ${targetregistry} -ot json | jq '.[] | select(.name=="'"$targetenv"'").identifier')
-
-        ${nifipath}/bin/cli.sh registry list-flows  -b ${targetbucketid} -u ${targetregistry} -ot json | jq '.[].name' | grep -i ${sourceflowname}
-        res=$?
-
-        if [[ $res -ne 0 ]]
-                then
-                        # Creating a new flow at target registry. It will get the new flow id
-                        newtargetflowid=$(${nifipath}/bin/cli.sh registry create-flow --flowName ${sourceflowname} -b ${targetbucketid} -u ${targetregistry})
-                        # Import the flow from json file to the target nifi registry
-                        targetflowversion=$(${nifipath}/bin/cli.sh registry import-flow-version -f ${newtargetflowid} --input flow.json -u ${targetregistry})
-                        ${nifipath}/bin/cli.sh nifi pg-import -f ${newtargetflowid} -b ${targetbucketid} -fv ${targetflowversion} -u ${targetnifi}
-                else
-                        newtargetflowid=$(${nifipath}/bin/cli.sh registry list-flows  -b ${targetbucketid} -u ${targetregistry} -ot json | jq '.[] | select(.name=="'"${sourceflowname}"'").identifier')
-                        targetflowversion=$(${nifipath}/bin/cli.sh registry import-flow-version -f ${newtargetflowid} --input flow.json -u ${targetregistry})
-                        targetpgid=$(${nifipath}/bin/cli.sh nifi pg-list -ot json -u ${targetnifi} | jq '.[] | select(.name=="'"${sourceflowname}"'").id')
-                        ${nifipath}/bin/cli.sh nifi pg-change-version -pgid ${targetpgid} -fv ${targetflowversion} -u ${targetnifi}
-                        ${nifipath}/bin/cli.sh nifi pg-enable-services -pgid ${targetpgid} -u ${targetnifi}
-                        ${nifipath}/bin/cli.sh nifi pg-start -pgid ${targetpgid} -u ${targetnifi}
-        fi
-
-done
+# Inport nifi flow into the target nifi instance
+${nifipath}/bin/cli.sh nifi pg-list -u ${targetnifi} -ot json | jq '.[].name' | grep -i ${sourceflowname}
+res=$?
+# If the flow is not created into the target nifi, then we need to create a new flow and later inport the flow 
+if [[ $res -ne 0 ]]
+  then
+    ${nifipath}/bin/cli.sh nifi pg-import -f ${sourceflowid} -b ${sourcebucketid} -fv ${sourceflowversion} -u ${targetnifi}
+  else 
+    targetpgid=$(${nifipath}/bin/cli.sh nifi pg-list -ot json -u ${targetnifi} | jq '.[] | select(.name=="'"${sourceflowname}"'").id')
+    ${nifipath}/bin/cli.sh nifi pg-change-version -pgid ${targetpgid} -fv ${sourceflowversion} -u ${targetnifi}
+    ${nifipath}/bin/cli.sh nifi pg-enable-services -pgid ${targetpgid} -u ${targetnifi}
+    ${nifipath}/bin/cli.sh nifi pg-start -pgid ${targetpgid} -u ${targetnifi}
+fi
